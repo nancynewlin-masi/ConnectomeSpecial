b0096285b6c2052cebe318174e21d0b4a0a1b146
nancynewlin-masi <86989160+nancynewlin-masi@users.noreply.github.com>
2024-02-05 16:42:40 -0600

    Update qa.py

--- CODE/qa.py
+++ CODE/qa.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import json
+import pandas as pd
 
 brain_file=sys.argv[1]
 roi_file=sys.argv[2]
@@ -31,18 +32,31 @@ connectome_data_2 = np.load(connectome_file_2)
 connectome_data_3 = np.load(connectome_file_3)
 
 #fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
-fig, axs = plt.subplots(3, 3, figsize=(15, 10))
-plt.figtext(0.01,0.85,content, fontsize='large')
-plt.subplots_adjust(left=0.2, right=0.9, bottom=0.1, top=0.85, wspace=0.2, hspace=0.4)
+fig, axs = plt.subplots(4, 3, figsize=(12, 12))
 
-r0c0=plt.subplot2grid((3,3), (0,0))
-r0c1=plt.subplot2grid((3,3), (0,1))
-r0c2=plt.subplot2grid((3,3), (0,2))
-r1c0=plt.subplot2grid((3,3), (1,0))
-r1c1=plt.subplot2grid((3,3), (1,1))
-r1c2=plt.subplot2grid((3,3), (1,2))
-r2c01=plt.subplot2grid((3,3), (2,0), colspan=2)
-r2c2=plt.subplot2grid((3,3), (2,2))
+for i in np.arange(0,4):
+    for j in np.arange(0,3):
+        axs[i,j].axis('off')
+
+
+
+plt.figtext(0.01,0.90,content, fontsize='large')
+plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.85, wspace=0.4, hspace=0.4)
+
+
+r0c0=plt.subplot2grid((4,3), (0,0))
+r0c1=plt.subplot2grid((4,3), (0,1))
+r0c2=plt.subplot2grid((4,3), (0,2))
+r1c0=plt.subplot2grid((4,3), (1,0))
+r1c1=plt.subplot2grid((4,3), (1,1))
+r1c2=plt.subplot2grid((4,3), (1,2))
+r2c0=plt.subplot2grid((4,3), (2,0))
+r2c1=plt.subplot2grid((4,3), (2,1))
+r2c2=plt.subplot2grid((4,3), (2,2))
+r2c01=plt.subplot2grid((4,3), (3,0), colspan=3)
+r0c0.axis('off')   
+r0c1.axis('off')   
+r0c2.axis('off')   
 # Load NIfTI files
 print("Loading images with nibabel...")
 brain_img = nib.load(brain_file)
@@ -72,9 +86,9 @@ print("Get some overlay...")
 
 print("Plot result...")
 # Plot the result
-r0c0.imshow(roi_slice, cmap='viridis', aspect='equal')
-r0c0.imshow(brain_slice, cmap='gray', alpha=0.7, aspect='equal')
-r0c0.set_title('Middle Brain Slice\n of B0 with ROI', fontsize=10)
+r0c0.imshow(np.rot90(roi_slice), cmap='viridis', aspect='equal')
+r0c0.imshow(np.rot90(brain_slice), cmap='gray', alpha=0.7, aspect='equal')
+r0c0.set_title('B0 with ROI seg - Axial\nR|L', fontsize=10)
 
 
 # Get the middle slice index
@@ -92,9 +106,9 @@ print("Get some overlay...")
 
 print("Plot result...")
 # Plot the result
-r0c1.imshow(roi_slice, cmap='viridis', aspect='equal', extent=(5,6,5,6))
-r0c1.imshow(brain_slice, cmap='gray', alpha=0.7, aspect='equal', extent=(5,6,5,6))
-r0c1.set_title('Middle Brain Slice\n of B0 with ROI', fontsize=10)
+r0c1.imshow(np.rot90(roi_slice), cmap='viridis', aspect='equal', extent=(5,6,5,6))
+r0c1.imshow(np.rot90(brain_slice), cmap='gray', alpha=0.7, aspect='equal', extent=(5,6,5,6))
+r0c1.set_title('B0 with ROI seg - Coronal\nP|A', fontsize=10)
 #fig = plt.figure(frameon=False)
 
 # Get the middle slice index
@@ -112,10 +126,11 @@ print("Get some overlay...")
 
 print("Plot result...")
 # Plot the result
-r0c2.imshow(roi_slice, cmap='viridis',aspect='equal', extent=(5,6,5,6))
-r0c2.imshow(brain_slice, cmap='gray', alpha=0.7, aspect='equal', extent=(5,6,5,6))
-r0c2.set_title('Middle Brain Slice\n of B0 with ROI', fontsize=10)
+r0c2.imshow(np.rot90(roi_slice), cmap='viridis',aspect='equal', extent=(5,6,5,6))
+r0c2.imshow(np.rot90(brain_slice), cmap='gray', alpha=0.7, aspect='equal', extent=(5,6,5,6))
+r0c2.set_title('B0 with ROI seg - Coronal\nR|L', fontsize=10)
 #fig = plt.figure(frameon=False)
+plt.axis('off')
 
 print("Plot result...")
 # Plot the result
@@ -125,33 +140,63 @@ import matplotlib.colors as colors
 r1c0.imshow(connectome_data_1, cmap='viridis',vmin=0.0, vmax=0.001*np.max(connectome_data_1))
 fig.colorbar(connectome1, ax=r1c0)
 r1c0.set_title('Connectome weighted by \n number of streamlines (0 to 0.001*max)', fontsize=10)
-
+plt.axis('off')
 connectome2 = r1c1.imshow(connectome_data_2, cmap='viridis')
 fig.colorbar(connectome2, ax=r1c1)
 r1c1.set_title('Connectome weighted by \n mean length of streamlines', fontsize=10)
-
+plt.axis('off')
 connectome3 = r1c2.imshow(connectome_data_3, cmap='viridis')
 fig.colorbar(connectome3, ax=r1c2)
 r1c2.set_title('Connectome weighted by \n fractional anisotropy (FA)', fontsize=10)
-r2c01.axis('off')
+plt.axis('off')
+
+
+
 with open(json_file, 'r') as file:
     data = json.load(file)
 # Create a list of tuples for table data
 table_data = [(metric, values[0]) for metric, values in data.items()]
-table = r2c01.table(cellText=table_data, colLabels=["Metric", "Value"], loc='center', cellLoc='center')
+absrange = ['[0,1]','[0,1]','[-1,1]','[0,1]','[0,inf]','[0,MaxStreamlines]','[0,inf]','[0,inf]','[0,1]', '[0,1]','[0,inf]','[0,inf]']
+exprange = ['[0,0.002]','[0.5,0.9]','[-0.01,0.01]','[0,40]','[0, 0.06]','[0, 10,000]','[50,90]','[50,90]','[0.7,0.9]','[0,1]','[0,250]','[0,10]']
+print("abs range shape:",np.shape(absrange))
+df1 = pd.DataFrame(table_data, columns=['Metric', 'Value'])
+print(df1)
+print([absrange, exprange])
+df2 = pd.DataFrame({'Absolute Range': absrange, 'Expected Range': exprange})
+print(df2)
+df = df1.join(df2, how='outer')
+print(df)
+print(df.shape)
+df = df.round(3)
+table = r2c01.table(cellText=df.reset_index().values, colLabels=df.reset_index().columns, loc='center', cellLoc='center')
+# bet centrality, modularity, assortativity, participation, clustering, nodal strength, local eff, glob eff, density, rich club, path length, edge count
+
 table.auto_set_font_size(False)
-table.set_fontsize(12)
+table.set_fontsize(9)
 
 # Adjust the layout for better visibility
 table.auto_set_column_width([0, 1])
 
 # Create a bar chart
-r2c2.title.set_text('Histogram of Connectome\nedge weights (NOS weighed)')    
+#r2c0.title.set_text('Histogram of Connectome\nedge weights (NOS weighed)',fontsize=10)    
+r2c0.set_title('Histogram of Connectome\nedge weights (NOS weighed)', fontsize=10)
 # r2c2.grid() 
-r2c2.hist(connectome_data_1.flatten(), bins=10)
-r2c2.set_xlabel("Frequency")
-r2c2.set_ylabel("NOS")
+r2c0.hist(connectome_data_1.flatten(), bins=10)
+r2c0.set_xlabel("Frequency")
+r2c0.set_ylabel("NOS")
 
+r2c1.set_title('Histogram of Connectome\nedge weights (Mean Length weighed)', fontsize=10)    
+# r2c2.grid() 
+r2c1.hist(connectome_data_2.flatten(), bins=10)
+r2c1.set_xlabel("Frequency")
+r2c1.set_ylabel("Mean Length")
+
+r2c2.set_title('Histogram of Connectome\nedge weights (Mean FA weighed)', fontsize=10)    
+# r2c2.grid() 
+r2c2.hist(connectome_data_3.flatten(), bins=10)
+r2c2.set_xlabel("Frequency")
+r2c2.set_ylabel("Mean FA")
+plt.axis('off')
 
 
 # Save the visualization as a PNG file
