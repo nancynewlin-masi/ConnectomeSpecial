f8d0ba52c333814109bf8f54bacc769e12140171
nancynewlin-masi <86989160+nancynewlin-masi@users.noreply.github.com>
2024-02-05 17:32:35 -0600

    Update main.sh

--- CODE/main.sh
+++ CODE/main.sh
@@ -294,4 +294,4 @@ fi
 echo "Completed Connectome special." >> ${OUTPUTDIR}/log.txt
 date >> ${OUTPUTDIR}/log.txt
 
-python /ConnectomeSpecial/CODE/qa.py /TEMP/b0.nii.gz ${SLANTSEG} ${OUTPUTDIR}/CONNECTOME_NUMSTREAM.npy ${OUTPUTDIR}/CONNECTOME_LENGTH.npy ${OUTPUTDIR}/CONNECTOME_FA.npy ${OUTPUTDIR}/graphmeasures.json ${OUTPUTDIR}/log.txt ${OUTPUTDIR}/ConnectomeQA.png
+python /ConnectomeSpecial/CODE/qa.py /ConnectomeSpecial/TEMP/b0.nii.gz ${ATLAS} ${OUTPUTDIR}/CONNECTOME_NUMSTREAM.npy ${OUTPUTDIR}/CONNECTOME_LENGTH.npy ${OUTPUTDIR}/CONNECTOME_FA.npy ${OUTPUTDIR}/graphmeasures.json ${OUTPUTDIR}/log.txt ${OUTPUTDIR}/ConnectomeQA.png
